ea4ec0766ef5f61aeb3ef234d55206db19d7fc97
Robert Waltham <rob@blockoftext.com>
2023-09-12 17:09:25 -0700

    leetcoding it up

---
+++ Swifty-Leetcode/remove-duplicates-sorted-list.swift
@@ -0,0 +1,83 @@
+//
+//  remove-duplicates-sorted-list.swift
+//  Swifty-Leetcode
+//
+//  Created by Robert Waltham on 2023-09-12.
+//
+
+import Foundation
+
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    public func toArray() -> [Int] {
+        var result = [Int]()
+        
+        var head = self
+        while head.next != nil {
+            result.append(head.val)
+            head = head.next!
+        }
+        result.append(head.val)
+
+        return result
+    }
+    
+    public static func fromArray(arr: [Int]?) -> ListNode? {
+        guard let arr else {
+            return nil
+        }
+        guard arr.count != 0 else {
+            return nil
+        }
+        
+        let head = ListNode(arr[0])
+        var cursor = head;
+        for i in 1..<arr.count {
+            cursor.next = ListNode(arr[i], nil)
+            
+            cursor = cursor.next!
+        }
+        
+        return head
+    }
+    
+}
+ 
+
+class Solution {
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+        guard let head else {
+            return nil
+        }
+        
+        guard head.next != nil else {
+            return head
+        }
+        
+        var slow = head
+        var fast = head.next!
+    
+        while fast.next != nil {
+            
+            if slow.val == fast.val {
+                print("dropping \(fast.val)")
+                fast = fast.next!
+                slow.next = fast
+            } else if fast.next != nil {
+                fast = fast.next!
+                slow = slow.next!
+            }
+        }
+        
+        if slow.val == fast.val {
+            slow.next = nil
+        }
+        
+        return head
+    }
+}
--- Swifty-LeetcodeTests/Swifty_LeetcodeTests.swift
+++ Swifty-LeetcodeTests/Swifty_LeetcodeTests.swift
@@ -10,27 +10,27 @@ import XCTest
 
 final class Swifty_LeetcodeTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testRemoveDuplicateSortedList() {
+        
+        
+        func testCase(input: [Int]?, expected: [Int]?) {
+            
+            let inputNode = ListNode.fromArray(arr: input)
+            let result = Solution.init().deleteDuplicates(inputNode)
+            let resultArr = result?.toArray()
+            
+            XCTAssertEqual(resultArr, expected, "expected \(String(describing: resultArr)) to equal \(String(describing: expected))")
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
         }
+        
+        testCase(input: nil, expected: nil)
+        testCase(input: [1], expected: [1])
+        testCase(input: [1, 2, 3], expected: [1, 2, 3])
+        testCase(input: [1, 1, 2, 2, 3], expected: [1, 2, 3])
+        testCase(input: [1, 2, 2, 3], expected: [1, 2, 3])
+        testCase(input: [1, 2, 3, 3], expected: [1, 2, 3])
+        testCase(input: [1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3], expected: [1, 2, 3])
+
     }
 
 }
